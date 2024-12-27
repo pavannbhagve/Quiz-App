1f90c343b2e4946bd311b53649b1f6422dd62966
PB <pb@gmail.com>
2024-12-27 14:04:41 +0530

    Questions add

---
+++ sc.js
@@ -0,0 +1,66 @@
+const questions = [
+    {
+        question: "Which is largest animal in the world",
+        Answers:[
+            {
+                text: "Shark" , correct: false
+            },
+            {
+                text: "Blue whale" , correct: true
+            },{
+                text: "Elephant" , correct: false
+            },{
+                text: "Giraffe" , correct: false
+            },
+
+        ]
+    },
+    {
+        question: "Which is largest desert in the world",
+        Answers:[
+            {
+                text: "Kalahari" , correct: false
+            },
+            {
+                text: " Gobi" , correct: false
+            },{
+                text: "Sahara" , correct: false
+            },{
+                text: "Antartica" , correct: true
+            },
+
+        ]
+    },
+    {
+        question: "Which is Smallest continent in the world",
+        Answers:[
+            {
+                text: "Asia" , correct: false
+            },
+            {
+                text: "Australia" , correct: true
+            },{
+                text: "Arctic" , correct: false
+            },{
+                text: "Africa" , correct: false
+            },
+
+        ]
+    },
+    {
+        question: "Which is smallest country in the world",
+        Answers:[
+            {
+                text: "Vatican City" , correct: true
+            },
+            {
+                text: "Bhutan" , correct: true
+            },{
+                text: "Nepal" , correct: false
+            },{
+                text: "Shri Lanka" , correct: false
+            },
+
+        ]
+    }
+]
